30dc6c58cbecab8ecb36225b9c80e84ba27307f3
VISHAAL-KUMAR-A <vkthedon123@gmail.com>
2025-03-06 21:43:34 +0530

    api fixing process

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import AdminDashboard from "./pages/AdminDashboard.jsx";
-export const apiUrl =
-  process.env.NODE_ENV == "production"
-    ? "https://systemic-altruism.onrender.com"
-    : "http://localhost:3000";
+// export const apiUrl =
+//   process.env.NODE_ENV == "production"
+//     ? "https://systemic-altruism.onrender.com"
+//     : "http://localhost:3000";
+export const apiUrl = "https://systemic-altruism.onrender.com";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
